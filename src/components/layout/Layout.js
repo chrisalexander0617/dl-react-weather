aad6e057bf128008610eaefa129682369e596e03
Chris <44784345+chrisalexander0617@users.noreply.github.com>
2021-10-14 19:43:10 -0400

    init commit

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from '../Nav'
+const Layout = ({children}) => {
+    return (
+        <>
+            <Nav />
+            <main>
+                {children}
+            </main>
+        </>
+    )
+}
+
+export default Layout
