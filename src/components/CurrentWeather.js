f6075d630da80260d1a0ecd8bb49104954f3a5f8
Chris <44784345+chrisalexander0617@users.noreply.github.com>
2021-10-14 20:30:55 -0400

    Added useEffect and api call

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -2,6 +2,20 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 function CurrentWeather(){
+    const [temp, setTemp] = useState()
+    const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${12}&lon=${12}&units=imperial&appid=${process.env.REACT_APP_API_KEY}`
+    
+    useEffect(() => {
+        axios.get(URL)
+        .then((res) =>{
+        setTemp(res.data)
+        console.log(res.data)
+        }).catch((err) => {
+            console.log(err)
+        })
+    },[temp])
+  
+
     return (
         <>
         <h1>Current Weather</h1>
